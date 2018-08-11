9f0d64df752ef8787fd3dfe1fa578702649ad68d
wickinot <27719504+wickinot@users.noreply.github.com>
2018-08-11 11:48:35 +1000

    first post

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var express = require('express'); 
+var express = require('express'); //adding the express module
+var fs = require('fs'); //adding the fs module
 // could call the express variable anything you want - common practice to use same name as the package.
 var app = express();
 
@@ -6,6 +7,20 @@ app.use(express.static("public"));
 //tells app to use this middleware function and point it at the the folder public
 //Whenever a GET request is received it will first check the public folder to see if a file
 //exists with the matching URL
+
+
+app.get('/get-posts', function(request, response){ //handler function for /get-posts
+	fs.readFile(__dirname+'/data/posts.json', function(error, data){
+		if(error){ //check if there's an error
+			console.log('Error reading posts.json: '+error); //if there is, log it and send it back as error response
+			response.status(500);
+			response.sent(error);
+		} else {
+			response.send(data.toString());// else convert the data buffer to a string and send that as a response
+		}
+	});
+});
+
 app.listen(8080, function() {
 	console.log('Server has started listening on port 8080.');
 });
