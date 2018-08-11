fd1fd9e622befb399797fe60b33c4f98b347a5b6
wickinot <27719504+wickinot@users.noreply.github.com>
2018-08-11 12:19:59 +1000

    added express-formidable

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
-var express = require('express'); //adding the express module
-var fs = require('fs'); //adding the fs module
-// could call the express variable anything you want - common practice to use same name as the package.
-var app = express();
+var express = require('express'); //adding the express module (middleware function)
+var formidable = require('express-formidable'); //add the express-formidable middleware
+var fs = require('fs'); //adding the fs module to interact with the file system on your computer
+
+var app = express();// could call the express variable anything you want - common practice to use same name as the package.
 
 app.use(express.static("public"));
 //tells app to use this middleware function and point it at the the folder public
 //Whenever a GET request is received it will first check the public folder to see if a file
 //exists with the matching URL
+app.use(formidable());
 
+app.post('/create-post', function(request, response){ //handler function
+	console.log(request.fields);
+});
 
 app.get('/get-posts', function(request, response){ //handler function for /get-posts
 	fs.readFile(__dirname+'/data/posts.json', function(error, data){
+		//fs.readfile() asunchronously reads the contents of a file
 		if(error){ //check if there's an error
 			console.log('Error reading posts.json: '+error); //if there is, log it and send it back as error response
 			response.status(500);
@@ -20,6 +26,7 @@ app.get('/get-posts', function(request, response){ //handler function for /get-p
 		}
 	});
 });
+
 
 app.listen(8080, function() {
 	console.log('Server has started listening on port 8080.');
