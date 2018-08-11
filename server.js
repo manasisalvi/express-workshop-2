dc591b1c8b2b1e604ccbd6f6716186ddc518ec82
wickinot <27719504+wickinot@users.noreply.github.com>
2018-08-11 11:34:51 +1000

    getting express middleware function running

--- server.js
+++ server.js
@@ -2,14 +2,10 @@ var express = require('express');
 // could call the express variable anything you want - common practice to use same name as the package.
 var app = express();
 
-app.get('/hello', function (request, response) {
-	response.send('Hello World!');
-})
-
-app.get('/chocolate', function (request, response) {
-	response.send('Mmmm, chocolate...');
-});
-
+app.use(express.static("public"));
+//tells app to use this middleware function and point it at the the folder public
+//Whenever a GET request is received it will first check the public folder to see if a file
+//exists with the matching URL
 app.listen(8080, function() {
 	console.log('Server has started listening on port 8080.');
 });
